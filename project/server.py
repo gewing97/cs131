95b45b35b3adba60f380c112e3bf5b5341ec7bf3
Graham Wing <gewing97@github.com>
2018-06-04 21:43:50 -0700

    added propagation routine and managing connections

--- project/server.py
+++ project/server.py
@@ -25,52 +25,65 @@ def talks_with(server_name):
         'Welsh' : ['Holiday']
     } [server_name]
 
+async def propagate_message(message):
+    composition = message.split(" ")
+    message_to_friends = "{0} {1}\n".format(message, sys.argv[1])
+    if len(composition) > 6:
+        up_stream = composition[6:]
+    else:
+        up_stream = []
+    for friend in talks_with(sys.argv[1]):
+        friend_port = get_port_num(friend)
+        if friend not in up_stream:
+            persistent_connection = False
+            for connected in connected_servers:
+                if friend_port == connected[0]:
+                    try:
+                        connected[2].write(message_to_friends.encode())
+                        await connected[2].drain()
+                        persistent_connection = True                        
+                    except Exception as e:
+                        print(e)
+                        connected_servers.remove(connected)
+                        persistent_connection = False
+            if not persistent_connection:
+                try:
+                    connected_servers.append(await connection_routine(friend_port, message_to_friends))
+                except:
+                    pass
+
+async def maintain_connections(up_stream_friends):
+    ports = []
+    for i in up_stream_friends:
+        ports.append(get_port_num(i))
+    for connected in connected_servers:
+        if connected[0] in ports:
+            ports.remove(connected[0])
+    for maintain in ports:
+        try:
+            reader, writer = await asyncio.open_connection('127.0.0.1', maintain, loop=loop)
+            connected_servers.append(maintain, reader, writer)        
+        except:
+            pass
+
+async def connection_routine(server_port, message):
+    reader, writer = await asyncio.open_connection('127.0.0.1', server_port, loop=loop)   
+    writer.write(message.encode())
+    await writer.drain()        
+    return server_port, reader, writer
+
+
+#def handle_iamat(message, writer):
 
 async def server_routine(reader, writer):
-    #print(writer.get_extra_info("socket"))
-    #print(writer.get_extra_info("peername"))
-    #print(writer.get_extra_info("sockname")[1])
     while True:
         try:
             received = await reader.readuntil(b'\n')
             received_decoded = received.decode().strip("\n").strip("\r")
             print('Received: %r' % received_decoded)
-            message_to_friends = "{0} heard {1}".format(sys.argv[1], received.decode())
-            for friend in talks_with(sys.argv[1]):
-                friend_port = get_port_num(friend)
-                if friend != received_decoded.split(" ")[0]:
-                    persistent_connection = False
-                    for connected in connected_servers:
-                        if friend_port == connected[0]:
-                            try:
-                                connected[2].write(message_to_friends.encode())
-                                await connected[2].drain()
-                                persistent_connection = True                        
-                            except Exception as e:
-                                print(e)
-                                connected_servers.remove(connected)
-                                persistent_connection = False
-                    if not persistent_connection:
-                        try:
-                            connected_servers.append(await connection_routine(friend_port, message_to_friends,loop))
-                        except:
-                            pass
+            await propagate_message(received_decoded)
         except:
             break
-    writer.close()
-
-async def connection_routine(server_port, message, loop):
-    reader, writer = await asyncio.open_connection('127.0.0.1', server_port, loop=loop)
-    print('%r' % message)        
-    writer.write(message.encode())
-    await writer.drain()        
-    return server_port, reader, writer
-
-async def connection_generator(servers, server_name, loop):
-    try:
-        await asyncio.ensure_future(connection_routine(get_port_num(servers[0]), server_name, loop))
-    except:
-        pass
 
 
 def main():
@@ -78,17 +91,12 @@ def main():
     routine = asyncio.start_server(server_routine, '127.0.0.1', server_port, loop=loop)
     server = loop.run_until_complete(routine)
 
-    #connection = loop.run_until_complete(connection_generator(talks_with(sys.argv[1]), sys.argv[1], loop))
-    #routine2 = asyncio.start_server(server_routine, '127.0.0.1', get_port_num('Hands'), loop=loop)
-    #server2 = loop.run_until_complete(routine2)
-
     try:
         loop.run_forever()
     except KeyboardInterrupt:
         pass
-    
+
     server.close()
-    #server2.close()
     loop.run_until_complete(server.wait_closed())
     loop.close()
 
