4ac7f59f1c7c07a11d0c35a1f33e646a1c07abaa
Graham Wing <gewing97@github.com>
2018-06-05 00:19:40 -0700

    fixed some bugs

--- project/server.py
+++ project/server.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-
+import time
 import asyncio
 import datetime
 import sys
 
 loop = asyncio.get_event_loop()
 connected_servers = []
+user_data = {}
+# user_name : [server,skew,lat,lon,time]
 
 def get_port_num(server_name):
     return {
@@ -72,16 +74,96 @@ async def connection_routine(server_port, message):
     await writer.drain()        
     return server_port, reader, writer
 
+def handle_latlon(lat_lon_str):
+    print(lat_lon_str)
+    if(lat_lon_str[0] == "+"):
+        lat_lon = lat_lon_str.strip("+")
+        print(lat_lon)
+        pos_split = lat_lon.split("+")
+        neg_split = lat_lon.split("-")
+        if(len(pos_split) == 2):
+            lat = float(pos_split[0])
+            lon = float(pos_split[1])
+        elif(len(neg_split) == 2):
+            lat = float(neg_split[0])
+            lon = float("-{0}".format(neg_split[1]))
+        else:
+            raise ValueError("invalid input")                                  
+    elif(lat_lon_str[0] == "-"):
+        lat_lon = lat_lon_str.strip("-")
+        pos_split = lat_lon.split("+")
+        neg_split = lat_lon.split("-")
+        if(len(pos_split) == 2):
+            lat = float("-{0}".format(pos_split[0]))
+            lon = float(pos_split[1])
+        elif(len(neg_split) == 2):
+            lat = float("-{0}".format(neg_split[0]))
+            lon = float("-{0}".format(neg_split[1]))
+        else:
+            raise ValueError("invalid input")         
+    else:              
+        raise ValueError("invalid input")
+    return lat, lon
 
-#def handle_iamat(message, writer):
+
+async def handle_iamat(message, writer, received_time):
+    try:
+        if len(message) == 4:
+            lat, lon = handle_latlon(message[2])
+            print(lat)
+            print(lon)
+            skew = received_time - float(message[3])
+            skew_str = "+{0}".format(skew) if skew >= 0 else "{0}".format(skew)
+            response = "AT {0} {1} {2}\n".format(sys.argv[1],skew_str, " ".join(message[1:]))   
+            user_data[message[1]] = [sys.argv[1], skew_str, lat, lon, message[3]]
+            writer.write(response.encode())
+            return 0
+        else:          
+            return 1
+    except Exception as e:
+        print(e)        
+        return 1
+
+async def handle_whatsat(message, writer):
+    try:
+        if len(message) == 4:
+            curr_user = user_data[message[1]]
+
+            lat = "+{0}".format(curr_user[2]) if curr_user[2] >= 0 else "{0}".format(curr_user[2])
+            lon = "+{0}".format(curr_user[3]) if curr_user[3] >= 0 else "{0}".format(curr_user[3])
+            lat_lon = "{0}{1}".format(lat,lon)
+
+            response = "AT {0} {1} {2} {3} {4}\n".format(curr_user[0],curr_user[1], message[1], lat_lon, curr_user[4])           
+            writer.write(response.encode())
+            return 0
+        else:
+            return 1
+    except:
+        return 1
+
 
 async def server_routine(reader, writer):
     while True:
         try:
+            received_time = time.time()
             received = await reader.readuntil(b'\n')
             received_decoded = received.decode().strip("\n").strip("\r")
+            received_decomp = received_decoded.split()
             print('Received: %r' % received_decoded)
-            await propagate_message(received_decoded)
+            error = 0
+            if (len(received_decomp) > 0):
+                if (received_decomp[0] == "IAMAT"):
+                    error = await handle_iamat(received_decomp, writer, received_time)
+                elif (received_decomp[0] == "WHATSAT"):
+                    error = await handle_whatsat(received_decomp, writer)
+                elif (received_decomp[0] == "AT"):
+                    await propagate_message(received_decoded)
+                else:
+                    error = 1
+            else:
+                error = 1
+            if error:
+                writer.write("? {0}".format(received.decode()).encode())
         except:
             break
 
